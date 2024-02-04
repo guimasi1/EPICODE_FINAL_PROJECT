a6e3f2d9158fc8ada2aef3a7d2e45879b07b822f
guimasi1 <143355576+guimasi1@users.noreply.github.com>
2024-02-04 16:25:03 +0100

    adds update by id and delete by id in PhysiotherapistsService

--- src/main/java/guidomasi/Final/Project/services/PhysiotherapistsService.java
+++ src/main/java/guidomasi/Final/Project/services/PhysiotherapistsService.java
@@ -2,7 +2,11 @@ package guidomasi.Final.Project.services;
 
 import guidomasi.Final.Project.entities.Patient;
 import guidomasi.Final.Project.entities.Physiotherapist;
+import guidomasi.Final.Project.enums.Gender;
+import guidomasi.Final.Project.enums.Specialization;
 import guidomasi.Final.Project.exceptions.NotFoundException;
+import guidomasi.Final.Project.payloads.patient.NewPatientDTO;
+import guidomasi.Final.Project.payloads.physiotherapist.NewPhysiotherapistDTO;
 import guidomasi.Final.Project.repositories.PhysiotherapistsDAO;
 import org.springframework.beans.factory.annotation.Autowired;
 import org.springframework.data.domain.Page;
@@ -28,6 +32,24 @@ public class PhysiotherapistsService {
     }
     public Physiotherapist findById(UUID id) throws NotFoundException {
         return physiotherapistsDAO.findById(id).orElseThrow(() -> new NotFoundException("Physiotherapist with id " + id + " not found!"));
+    }
+
+    public void deleteById(UUID uuid) {
+        Physiotherapist found = this.findById(uuid);
+        physiotherapistsDAO.delete(found);
+    }
+
+    public Physiotherapist findByIdAndUpdate(UUID uuid, NewPhysiotherapistDTO body) {
+        Physiotherapist found = this.findById(uuid);
+        found.setEmail(body.email());
+        found.setPassword(body.password());
+        found.setFirstName(body.firstName());
+        found.setLastName(body.lastName());
+        found.setPhoneNumber(body.phoneNumber());
+        found.setDateOfBirth(body.dateOfBirth());
+        Specialization specialization = Specialization.valueOf(body.specialization().toUpperCase());
+        found.setSpecialization(specialization);
+        return physiotherapistsDAO.save(found);
     }
 
 
